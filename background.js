5d1d89bea4b80fe15ace83b6c937b5d59af7f40a
stevew2131BypassArchiveIs <70620486+stevew2131BypassArchiveIs@users.noreply.github.com>
2020-09-14 22:01:41 -0400

    Added support for new Request Header blocking.
    Site now checks for request headers. This takes care of that.

--- background.js
+++ background.js
@@ -6,3 +6,19 @@ chrome.cookies.remove({url: "https://archive.fo", name: "unsupported-browser"});
 chrome.cookies.remove({url: "https://archive.li", name: "unsupported-browser"});
 chrome.cookies.remove({url: "https://archive.md", name: "unsupported-browser"});
 chrome.cookies.remove({url: "https://archive.ph", name: "unsupported-browser"});
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    for (var i = details.requestHeaders.length - 1; i >= 0; i--) {
+      if (details.requestHeaders[i].name.toLowerCase() === 'User-Agent'.toLowerCase()) {
+        details.requestHeaders.splice(i, 1);
+      }
+	  if (details.requestHeaders[i].name.toLowerCase() === 'cookie'.toLowerCase()) {
+        details.requestHeaders.splice(i, 1);
+      }
+    }
+    return { requestHeaders: details.requestHeaders };
+  },
+  {urls: ["https://archive.is/*", "https://archive.vn/*", "https://archive.today/*", "https://archive.fo/*", "https://archive.li/*", "https://archive.md/*", "https://archive.ph/*"]},
+  [ 'blocking', 'requestHeaders', 'extraHeaders']
+);
